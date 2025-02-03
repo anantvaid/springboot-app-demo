abd701453f9b1a645fd3e1b9921d73edb06d5f88
Ubuntu <ubuntu@ip-172-31-2-114.ap-south-1.compute.internal>
2025-02-03 12:40:46 +0000

    Changes to EC2 IP

--- fullstack-front/src/helpers/axios_helper.js
+++ fullstack-front/src/helpers/axios_helper.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'http://localhost';
+axios.defaults.baseURL = 'http://13.233.201.127';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 const userUrl = "/api/v1/users";
